122e8af96088de61af5836397df973f11dc65db5
Ophelieyagh <ophelie.yagh@orange.fr>
2026-05-16 19:47:45 +0200

    arena end

--- levels/lll/texscroll.inc.c
+++ levels/lll/texscroll.inc.c
@@ -1,22 +1,22 @@
 void scroll_lll_dl_arena_mesh_layer_1_vtx_1() {
 	int i = 0;
 	int count = 57;
-	int width = 32 * 0x20;
+	int height = 32 * 0x20;
 
-	static int currentX = 0;
-	int deltaX;
+	static int currentY = 0;
+	int deltaY;
 	Vtx *vertices = segmented_to_virtual(lll_dl_arena_mesh_layer_1_vtx_1);
 
-	deltaX = (int)(2.0 * 0x20) % width;
+	deltaY = (int)(1.0 * 0x20) % height;
 
-	if (absi(currentX) > width) {
-		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
 	}
 
 	for (i = 0; i < count; i++) {
-		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
 	}
-	currentX += deltaX;
+	currentY += deltaY;
 }
 
 void scroll_lll() {
