12199bf2630f0341c5d3ebc27b9c30bf2462ff45
Ophelieyagh <ophelie.yagh@orange.fr>
2026-05-16 22:17:03 +0200

    puzzles

--- levels/lll/texscroll.inc.c
+++ levels/lll/texscroll.inc.c
@@ -1,3 +1,24 @@
+void scroll_lll_dl_volcano_mesh_layer_1_vtx_3() {
+	int i = 0;
+	int count = 39;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(lll_dl_volcano_mesh_layer_1_vtx_3);
+
+	deltaY = (int)(1.0 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
 void scroll_lll_dl_arena_mesh_layer_1_vtx_1() {
 	int i = 0;
 	int count = 57;
@@ -20,5 +41,6 @@ void scroll_lll_dl_arena_mesh_layer_1_vtx_1() {
 }
 
 void scroll_lll() {
+	scroll_lll_dl_volcano_mesh_layer_1_vtx_3();
 	scroll_lll_dl_arena_mesh_layer_1_vtx_1();
 };
--- levels/lll/texscroll.inc.h
+++ levels/lll/texscroll.inc.h
@@ -1,2 +1,3 @@
+extern void scroll_lll_dl_volcano_mesh_layer_1_vtx_3();
 extern void scroll_lll_dl_arena_mesh_layer_1_vtx_1();
 extern void scroll_lll();
